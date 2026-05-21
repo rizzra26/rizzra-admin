bc5749d61b10bc1037c2dd65253375f3f0a50053
rizzra26 <rizukirimu@gmail.com>
2026-05-21 23:11:01 +0700

    deploy: change nitro to vercel on nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@pinia/nuxt'],
+  css: ["~/assets/css/main.css"],
+  modules: ["@pinia/nuxt"],
   vite: {
     plugins: [tailwindcss()],
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:8080/api/v1',
+      apiBaseUrl:
+        process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api/v1",
     },
   },
-  compatibilityDate: '2026-05-15',
-})
+  nitro: {
+    preset: "vercel",
+  },
+  compatibilityDate: "2026-05-15",
+});
